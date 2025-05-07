ebb85ed23c661691e84017d8db4aa62fb1c15795
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-07 11:08:51 +0530

    update modules/logger.go

--- modules/logger.go
+++ modules/logger.go
@@ -21,6 +21,11 @@ func init() {
 }
 
 func AddedToGroups(b *gotgbot.Bot, ctx *ext.Context) error {
+    if r, e :=  database.IsLoggerEnabled(); !r {
+        orCont(e)
+        
+    }
+    }
 	text := fmt.Sprintf(
 		`Hello 👋 I'm <b>%s</b>, here to help keep the chat transparent and secure.
 
