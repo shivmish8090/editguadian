48f139d429cf5f35dc7147a4b8994c73abe1c9e4
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-10 14:54:02 +0530

    update modules/stats.go

--- modules/stats.go
+++ modules/stats.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 
 	"main/config"
@@ -34,6 +33,6 @@ func stats(b *gotgbot.Bot, ctx *ext.Context) error {
 		text += fmt.Sprintf("Total Users: %d\n", len(users))
 	}
 
-	_, err := ctx.EffectiveMessage.Reply(b, text, nil)
+	_, err := ctx.EffectiveMessage.Reply(b, text, &gotgbot.SendMessageOpts{ParseMode:   "HTML"})
 	return orCont(err)
 }
