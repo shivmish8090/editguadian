93f96ca9cf75346a06cbe8a9acde5cea04d6b011
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-18 17:54:23 +0530

    Create links.go

---
+++ modules/links.go
@@ -0,0 +1,23 @@
+package modules
+
+func init() {
+        Register(handlers.NewMessage(func(m *gotgbot.Message) bool { 
+if m.Entities == nil {
+
+return false
+}
+
+if slices.ContainsFunc(m.Entities, func(entity gotgbot.MessageEntity) bool {
+		return slices.Contains([]string{"text_link", "url"},entity.Type) 
+	})
+
+}, DeleteLinkMessage))
+}
+
+func DeleteLinkMessage(b *gotgbot.Bot, ctx *ext.Context) error {
+
+m := ctx.EffectiveMessage
+
+m.Delete(b, nil)
+
+}
