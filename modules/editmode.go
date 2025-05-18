873a0eb066f00927b74c8d57c9fae516238d2425
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-18 11:49:24 +0530

    Update editmode.go

--- modules/editmode.go
+++ modules/editmode.go
@@ -74,7 +74,7 @@ func EditMode(b *gotgbot.Bot, ctx *ext.Context) error {
 		return err
 	}
 
-	if !helpers.Contains(admins, ctx.EffectiveUser.Id) {
+	if !slices.Contains(admins, ctx.EffectiveUser.Id) {
 		b.SendMessage(ctx.EffectiveChat.Id, "Access denied: Only group admins can use this command.\n\nIf you are an admin, please use /reload to refresh the admin list.", nil)
 		return Continue
 	}
