b01fd3f778d0355f29cf00affa5937fbc513fd3e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-18 11:51:21 +0530

    Update misc.go

--- config/helpers/misc.go
+++ config/helpers/misc.go
@@ -2,15 +2,6 @@ package helpers
 
 import "sync"
 
-func Contains[T comparable](slice []T, item T) bool {
-	for _, v := range slice {
-		if v == item {
-			return true
-		}
-	}
-	return false
-}
-
 func LoadTyped[T any](cache *sync.Map, key string) (T, bool) {
 	var zero T
 	if val, ok := cache.Load(key); ok {
