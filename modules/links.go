546a82ebcd4dc01dfa7b8a4d1bad9e23784edbf0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-19 13:28:09 +0530

    Update links.go

--- modules/links.go
+++ modules/links.go
@@ -8,17 +8,6 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
-func init() {
-	Register(handlers.NewMessage(func(m *gotgbot.Message) bool {
-		if m.Entities == nil {
-			return false
-		}
-		return slices.ContainsFunc(m.Entities, func(entity gotgbot.MessageEntity) bool {
-			return entity.Type == "url"
-		})
-	}, DeleteLinkMessage))
-}
-
 func DeleteLinkMessage(b *gotgbot.Bot, ctx *ext.Context) error {
 	m := ctx.EffectiveMessage
 
