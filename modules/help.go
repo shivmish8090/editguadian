b660431bc257a046bed78dcac497fa178e7fc936
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-21 20:50:54 +0530

    Update help.go

--- modules/help.go
+++ modules/help.go
@@ -14,6 +14,7 @@ func init() {
 }
 
 func helpCB(b *gotgbot.Bot, ctx *ext.Context) error {
+btn := &buttons.Button{}
 	keyboard := gotgbot.InlineKeyboardMarkup{
 		InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
 			{
